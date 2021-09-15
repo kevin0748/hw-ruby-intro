eaf2acdcd53477ccd1664f79a70ab922203a9c99
EC2 Default User <ec2-user@ip-172-31-41-161.us-east-2.compute.internal>
2021-09-15 07:01:19 +0000

    part1 & part2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,90 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each do |val|
+    sum += val
+  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  len = arr.length()
+  if len < 1
+    return 0
+  elsif len == 1
+    return arr[0]
+  end
+
+  maxA = arr.min() - 1
+  maxB = arr.min() - 1
+
+  arr.each do |val|
+    if val >= maxA
+      maxB = maxA
+      maxA = val
+    elsif val >= maxB
+      maxB = val
+    end
+  end
+
+  return maxA + maxB
+
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  seen = {}
+  arr.each do |val|
+    if seen.has_key?(n-val)
+      return true
+    end
+    seen[val] = 1
+  end
+
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.length() == 0
+      return false
+  end
+  
+  if s.match(/^[bcdfghjklmnpqrstvwxyz]/i)
+    return true
+  end
+
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  # check s is valid binary
+  if s.length() == 0
+      return false
+  end
+  s.each_char do |ch|
+    if ch != "0" && ch != "1"
+      return false
+    end
+  end
+
+  bit = 0
+  s.reverse.each_char do |ch|
+    if bit >= 2
+      return true
+    end
+
+    if ch != "0"
+      return false
+    end
+
+    bit += 1
+  end
 end
 
 # Part 3
